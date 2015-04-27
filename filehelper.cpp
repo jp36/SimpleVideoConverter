03e1f85bf223a2892f8e7eca8828ed4f25d74587
jp36 <jpettitt2@gmail.com>
2015-04-27 18:59:30 -0500

    untested, added some of the fileHelper functionality

--- filehelper.cpp
+++ filehelper.cpp
@@ -20,3 +20,42 @@ bool FileHelper::fileExists(QString path)
     return QDir(path).exists();
 }
 
+void FileHelper::start(QString argumentString)
+{
+    ffmpegProcess = new QProcess(this);
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+    ffmpegProcess->setProcessEnvironment(env);
+    connect(ffmpegProcess, &QProcess::started, this, &FileHelper::processStarted);
+    connect(ffmpegProcess, &QProcess::readyReadStandardOutput, this, &FileHelper::readyReadStandardOutput);
+    connect(ffmpegProcess, &QProcess::readyReadStandardError, this, &FileHelper::readyReadStandardError);
+    connect(ffmpegProcess, (void (QProcess::*)(int,QProcess::ExitStatus))(&QProcess::finished), this, &FileHelper::handleFinish);
+    qDebug() << QDir::currentPath()+"ffmpeg.exe" << argumentString;
+    //    ffmpegProcess.start(QDir::currentPath()+"ffmpeg.exe", argumentString);
+}
+
+QString FileHelper::getStandardOutput()
+{
+    return ffmpegProcess->readAllStandardOutput();
+}
+
+QString FileHelper::getStandardError()
+{
+    return ffmpegProcess->readAllStandardError();
+}
+
+void FileHelper::cancel()
+{
+    if(ffmpegProcess!=NULL && ffmpegProcess->isOpen())
+        ffmpegProcess->close();
+
+    ffmpegProcess->deleteLater();
+    ffmpegProcess = NULL;
+}
+
+void FileHelper::handleFinish(int exitCode, QProcess::ExitStatus exitStatus)
+{
+    ffmpegProcess->deleteLater();
+    ffmpegProcess = NULL;
+    emit encodingFinished();
+}
+
--- filehelper.h
+++ filehelper.h
@@ -2,6 +2,7 @@
 #define FILEHELPER_H
 
 #include <QObject>
+#include <QProcess>
 
 class FileHelper : public QObject
 {
@@ -12,6 +13,24 @@ public:
     ~FileHelper();
 
     Q_INVOKABLE bool fileExists(QString path);
+
+public slots:
+    void start(QString argumentString);
+    QString getStandardOutput();
+    QString getStandardError();
+    void cancel();
+
+protected slots:
+    void handleFinish(int exitCode, QProcess::ExitStatus exitStatus);
+
+signals:
+   void processStarted();
+   void readyReadStandardOutput();
+   void readyReadStandardError();
+   void encodingFinished();
+
+private:
+   QProcess* ffmpegProcess;
 };
 
 #endif // FILEHELPER_H
